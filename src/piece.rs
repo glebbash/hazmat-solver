9b9528f3dd6a889241800874b9b245c0e779b50e
glebbash <glebbash@gmail.com>
2020-07-09 13:34:53 +0200

    piece.get

--- src/piece.rs
+++ src/piece.rs
@@ -7,12 +7,12 @@ use std::io::prelude::*;
 pub struct Piece {
     pub width: usize,
     pub height: usize,
-    pub data: Vec<Vec<bool>>,
+    pub data: Vec<bool>,
     pub color: u32,
 }
 
 impl Piece {
-    pub fn new(width: usize, height: usize, data: Vec<Vec<bool>>, color: u32) -> Self {
+    pub fn new(width: usize, height: usize, data: Vec<bool>, color: u32) -> Self {
         Piece {
             width,
             height,
@@ -25,28 +25,35 @@ impl Piece {
         let f = File::open(format!("data/{}.piece", color))?;
 
         let mut data = vec![];
-        let mut row = vec![];
+        let mut width = 0;
+        let mut height = 1;
+        let mut i = 0;
 
         for c in f.bytes().map(|b| b.unwrap() as char) {
             if c == '\n' {
-                data.push(row);
-                row = vec![];
+                width = i;
+                height += 1;
+                i = 0;
             } else {
-                row.push(c != ' ');
+                data.push(c != ' ');
             }
+            i += 1;
         }
-        data.push(row);
 
-        Ok(Piece::new(data[0].len(), data.len(), data, color))
+        Ok(Piece::new(width - 1, height, data, color))
+    }
+
+    pub fn get(&self, i: usize, j: usize) -> bool {
+        unsafe { *self.data.get_unchecked(self.width * i + j) }
     }
 }
 
 impl fmt::Display for Piece {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let c = std::char::from_digit(self.color, 10).unwrap();
-        for row in self.data.iter() {
-            for b in row {
-                f.write_char(if *b { c } else { '.' })?;
+        for i in 0..self.height {
+            for j in 0..self.width {
+                f.write_char(if self.get(i, j) { c } else { '.' })?;
             }
             f.write_char('\n')?;
         }
